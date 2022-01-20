6f2f4064af511995ed3cd5983cb21be6650c9e3a
Borduas Godefroy <Godefroy@decinfo.cvm>
2022-01-20 14:28:38 -0500

    Correction du flux

---
+++ Assets/Scripts/EnnemyPatrol.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class EnnemyPatrol : MonoBehaviour
+{
+    /// <summary>
+    /// Vitesse de l'objet en patrouille
+    /// </summary>
+    [SerializeField]
+    private float _vitesse = 3f;
+    /// <summary>
+    /// Liste de GO représentant les points à atteindre
+    /// </summary>
+    [SerializeField]
+    private Transform[] _points;
+    /// <summary>
+    /// Référence vers la cible actuelle de l'objet
+    /// </summary>
+    private Transform _cible;
+    /// <summary>
+    /// Permet de connaître la position actuelle de la cible dans le tableau
+    /// </summary>
+    private int _indexPoint;
+    /// <summary>
+    /// Seuil où l'objet change de cible de déplacement
+    /// </summary>
+    private float _distanceSeuil = 0.3f;
+    /// <summary>
+    /// Référence vers le sprite Renderer
+    /// </summary>
+    private SpriteRenderer _sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sr = this.GetComponent<SpriteRenderer>();
+        _indexPoint = 0;
+        _cible = _points[_indexPoint];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = _cible.position - this.transform.position;
+        this.transform.Translate(direction.normalized * _vitesse * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(this.transform.position, _cible.position) < _distanceSeuil)
+        {
+            _indexPoint = (++_indexPoint) % _points.Length;
+            _cible = _points[_indexPoint];
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        // Ligne entre les cibles
+        for (int i = 0; i < _points.Length - 1; i++)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(_points[i].position, 
+                _points[i + 1].position);
+        }
+
+        // Ligne entre l'ennemi et la cible
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(this.transform.position, _cible.position);
+    }
+#endif
+}
