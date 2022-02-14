de7fa368741b804d66de9703e4a83f144548ef86
Borduas Godefroy <Godefroy@decinfo.cvm>
2022-02-14 15:54:23 -0500

    Ajout trame sonore et AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Représente un élément audio avec un nom (étiquette)
+/// </summary>
+[System.Serializable]
+struct AudioElement
+{
+    public string Nom;
+    public AudioClip Clip;
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    /// <summary>
+    /// Représente la playliste de la trame sonore
+    /// </summary>
+    [SerializeField]
+    private AudioElement[] _playlist;
+
+    /// <summary>
+    /// Référence vers l'audio source de l'objet
+    /// </summary>
+    private AudioSource _source;
+
+    private void Start()
+    {
+        // Récupère la référence
+        _source = this.gameObject.GetComponent<AudioSource>();
+        // Joue la musique pour la scène
+        AudioClip clip = null;
+        foreach (AudioElement clipData in _playlist)
+        {
+            if (clipData.Nom.Equals(SceneManager.GetActiveScene().name))
+                clip = clipData.Clip;
+        }
+        if (clip != null)
+            _source.clip = clip;
+        else
+            _source.clip = _playlist[0].Clip;
+
+        _source.Play();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -97,14 +97,15 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log($"Score : {this._playerData.Score}");
-        Debug.Log($"Vie : {this._playerData.Vie}, Énergie : {this._playerData.Energie}");
+        //Debug.Log($"Score : {this._playerData.Score}");
+        //Debug.Log($"Vie : {this._playerData.Vie}, Énergie : {this._playerData.Energie}");
         //string msg = string.Empty;
         //foreach (string chest in PlayerData.ListeCoffreOuvert)
         //{
         //    msg += chest + ";";
         //}
         //Debug.Log($"ChestList : {msg}");
+        this.gameObject.transform.position = Camera.main.transform.position;
     }
     #endregion
     public void RechargerNiveau()
