d3b6cb01358d1b160c70c1a3f28995d63b04a279
Godefroy Borduas <godefroy.borduas@gmail.com>
2022-02-13 19:52:49 -0500

    Remplacement de DES par AES

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -61,15 +61,16 @@ public class GameManager : MonoBehaviour
 
     public IEnumerator SaveData(PlayerData data)
     {
-        //using (StreamWriter stream = new StreamWriter(
-        //    Path.Combine(Application.persistentDataPath, "savedata.json"),
-        //    false, System.Text.Encoding.UTF8))
-        //{
-        //    stream.Write(PlayerDataJson.WriteJson(data));
-        //    Debug.Log(Path.Combine(Application.persistentDataPath, "savedata.json"));
-        //}
-        DataManipulator manipulator = new DataManipulator();
-        manipulator.Encrypt(PlayerDataJson.WriteJson(data), Path.Combine(Application.persistentDataPath, "savedata_encrypt.json"));
+        using (StreamWriter stream = new StreamWriter(
+            Path.Combine(Application.persistentDataPath, "savedata_encrypt.json"),
+            false, System.Text.Encoding.UTF8))
+        {
+            DataManipulator manipulator = new DataManipulator();
+            stream.Write(manipulator.Encrypt(PlayerDataJson.WriteJson(data)));
+            Debug.Log(Path.Combine(Application.persistentDataPath, "savedata_encrypt.json"));
+        }
+        //DataManipulator manipulator = new DataManipulator();
+        //manipulator.Encrypt(PlayerDataJson.WriteJson(data), Path.Combine(Application.persistentDataPath, "savedata_encrypt.json"));
         yield return new WaitForEndOfFrame();
     }
 
@@ -78,14 +79,14 @@ public class GameManager : MonoBehaviour
         string path = Path.Combine(Application.persistentDataPath, "savedata_encrypt.json");
         if (File.Exists(path))
         {
-            //using (StreamReader stream = new StreamReader(
-            //Path.Combine(Application.persistentDataPath, "savedata.json"),
-            //System.Text.Encoding.UTF8))
-            //{
-            //    this._playerData = PlayerDataJson.ReadJson(stream.ReadToEnd());
-            //}
-            DataManipulator manipulator = new DataManipulator();
-            this._playerData = manipulator.Decrypt(path);
+            using (StreamReader stream = new StreamReader(path,
+            System.Text.Encoding.UTF8))
+            {
+                DataManipulator manipulator = new DataManipulator();
+                this._playerData = PlayerDataJson.ReadJson(manipulator.Decrypt(stream.ReadToEnd()));
+            }
+            //DataManipulator manipulator = new DataManipulator();
+            //this._playerData = manipulator.Decrypt(path);
         }
         else
         {
--- Assets/Scripts/Utils/DataManipulator.cs
+++ Assets/Scripts/Utils/DataManipulator.cs
@@ -1,6 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
-using System.IO;
+using System;
 
 /// <summary>
 /// Permet la manipulation des données pour l'encryptage
@@ -8,61 +8,50 @@ using System.IO;
 public sealed class DataManipulator
 {
 
-    private byte[] data_key;
-    private byte[] data_pkey;
+    private string key = "XCtmd6zUnPVgT9dW6vkuyNRbxsjvQqpM";
+    private string iv = "QyEgUUI9OyLmUA8a";
 
-    public DataManipulator() {
-        string key = "A#NaOqBC";
-        data_key = Encoding.UTF8
-            .GetBytes(key);
-        data_pkey = Encoding.UTF8
-            .GetBytes("f06a78bd586d05323e2d9b620743d3b1");
+    /// <summary>
+    /// Permet d'encrypter une chaîne de caractère
+    /// </summary>
+    /// <param name="inputData">Chaîne à encrypter</param>
+    /// <returns>Chaîne encrypter</returns>
+    public string Encrypt(string inputData)
+    {
+        AesCryptoServiceProvider AEScryptoProvider = new AesCryptoServiceProvider();
+        AEScryptoProvider.BlockSize = 128;
+        AEScryptoProvider.KeySize = 256;
+        AEScryptoProvider.Key = ASCIIEncoding.ASCII.GetBytes(key);
+        AEScryptoProvider.IV = ASCIIEncoding.ASCII.GetBytes(iv);
+        AEScryptoProvider.Mode = CipherMode.CBC;
+        AEScryptoProvider.Padding = PaddingMode.PKCS7;
+
+        byte[] txtByteData = ASCIIEncoding.ASCII.GetBytes(inputData);
+        ICryptoTransform trnsfrm = AEScryptoProvider.CreateEncryptor(AEScryptoProvider.Key, AEScryptoProvider.IV);
+
+        byte[] result = trnsfrm.TransformFinalBlock(txtByteData, 0, txtByteData.Length);
+        return Convert.ToBase64String(result);
     }
 
-    public void Encrypt(string plainText, string path)
+    /// <summary>
+    /// Permet de décrypter une chaîne de caractère
+    /// </summary>
+    /// <param name="inputData">Chaîne à décrypter</param>
+    /// <returns>Chaîne décrypter</returns>
+    public string Decrypt(string inputData)
     {
-        using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
-        {
-            ICryptoTransform encrypto = des.CreateEncryptor(data_key, data_pkey);
+        AesCryptoServiceProvider AEScryptoProvider = new AesCryptoServiceProvider();
+        AEScryptoProvider.BlockSize = 128;
+        AEScryptoProvider.KeySize = 256;
+        AEScryptoProvider.Key = ASCIIEncoding.ASCII.GetBytes(key);
+        AEScryptoProvider.IV = ASCIIEncoding.ASCII.GetBytes(iv);
+        AEScryptoProvider.Mode = CipherMode.CBC;
+        AEScryptoProvider.Padding = PaddingMode.PKCS7;
 
-            using (MemoryStream msEncrypt = new MemoryStream())
-            {
-                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encrypto, CryptoStreamMode.Write))
-                {
-                    using (StreamWriter streamWriter = new StreamWriter(path))
-                    {
-                        byte[] cypto = Encoding.UTF8.GetBytes(plainText);
-                        csEncrypt.Write(cypto, 0, cypto.Length);
-                        csEncrypt.FlushFinalBlock();
-                        streamWriter.Write(Encoding.UTF8.GetString(msEncrypt.ToArray()));
-                    }
-                }
-            }
-        }
-    }
+        byte[] txtByteData = Convert.FromBase64String(inputData);
+        ICryptoTransform trnsfrm = AEScryptoProvider.CreateDecryptor();
 
-    public PlayerData Decrypt(string path)
-    {
-        string json = string.Empty;
-        using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
-        {
-            ICryptoTransform encrypto = des.CreateEncryptor(data_key, data_pkey);
-
-            using (MemoryStream msEncrypt = new MemoryStream())
-            {
-                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encrypto, CryptoStreamMode.Write))
-                {
-                    using (StreamReader streamReader = new StreamReader(path))
-                    {
-                        byte[] inputDecrypt = Encoding.UTF8.GetBytes(streamReader.ReadToEnd());
-                        csEncrypt.Write(inputDecrypt, 0, inputDecrypt.Length);
-                        csEncrypt.FlushFinalBlock();
-                        json = Encoding.UTF8.GetString(msEncrypt.ToArray());
-                    }
-                }
-            }
-        }
-
-        return PlayerDataJson.ReadJson(json);
+        byte[] result = trnsfrm.TransformFinalBlock(txtByteData, 0, txtByteData.Length);
+        return ASCIIEncoding.ASCII.GetString(result);
     }
 }
