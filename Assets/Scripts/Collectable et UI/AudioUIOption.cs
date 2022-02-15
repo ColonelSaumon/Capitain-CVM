896636fbbab0c9c80c9734c91209e1524f367616
Borduas Godefroy <Godefroy@decinfo.cvm>
2022-02-15 16:11:42 -0500

    Gestion AudioMixer

---
+++ Assets/Scripts/Collectable et UI/AudioUIOption.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class AudioUIOption : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer _audioMixer;
+    [SerializeField]
+    private string _parameter;
+
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = this.gameObject.GetComponent<Slider>();
+        _slider.onValueChanged.AddListener(ChangeVolume);
+    }
+
+    public void ChangeVolume(float value)
+    {
+        _audioMixer.SetFloat(_parameter, _slider.value);
+    }
+}
